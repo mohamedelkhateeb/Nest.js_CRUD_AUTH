3f5aba1eba51b2ca332f6cf3a32927cf72c53ec7
mohamedelkhateeb <mohamedelkhteeb74@gmail.com>
2024-07-09 01:35:46 +0300

    -Establish Post and Get Endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
-import { TasksModule } from './tasks/tasks.module';
+import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
+
+@Module({
+  imports: [],
+  controllers: [TasksController],
+  providers: [TasksService],
+})
 
 export class AppModule {}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private tasksService:TasksService ){}
+  constructor(private tasksService: TasksService) {}
+
+  @Get('getAll')
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post('create')
+  createTask(@Body() body): Task {
+    return this.tasksService.createTask(body.title, body.description);
+  }
 }
