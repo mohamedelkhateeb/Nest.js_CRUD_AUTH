0652610594bbd57e85b76323c52ad32bbe03889c
mohamedelkhateeb <mohamedelkhteeb74@gmail.com>
2024-07-09 02:30:47 +0300

    Add DTO to the project

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './DTOs/create-task.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -10,9 +19,22 @@ export class TasksController {
   getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getById(id);
+  }
 
   @Post('create')
-  createTask(@Body() body): Task {
-    return this.tasksService.createTask(body.title, body.description);
+  createTask(@Body() objectDTO: CreateTaskDto): Task {
+    return this.tasksService.createTask(objectDTO);
+  }
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
+  }
+
+  @Put('/status/:id')
+  changeStatus(@Param('id') id: string, @Body('status') status: TaskStatus) {
+    return this.tasksService.changeStatus(id, status);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { v4 as uuidv4 } from 'uuid';
+import { CreateTaskDto } from './DTOs/create-task.dto';
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
@@ -9,7 +10,13 @@ export class TasksService {
     return this.tasks;
   }
 
-  createTask(title: string, description: string): Task {
+  getById(id: string): Task {
+    const task = this.tasks.find((task) => task.id == id);
+    return task;
+  }
+
+  createTask(objectDTO: CreateTaskDto): Task {
+    const { title, description } = objectDTO;
     const task: Task = {
       id: uuidv4(),
       title,
@@ -17,6 +24,14 @@ export class TasksService {
       status: TaskStatus.DONE,
     };
     this.tasks.push(task);
+    return task;
+  }
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+  changeStatus(id: string, status: TaskStatus): Task {
+    const task = this.getById(id);
+    task.status = status;
     return task;
   }
 }
